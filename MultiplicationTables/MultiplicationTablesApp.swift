8ec57dd8426bacc951436ea829ece58e2106676b
Monty Harper <132530279+MontyHarper@users.noreply.github.com>
2023-08-08 15:42:50 -0500

    Initial Commit

---
+++ MultiplicationTables/MultiplicationTablesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplicationTablesApp.swift
+//  MultiplicationTables
+//
+//  Created by Monty Harper on 8/8/23.
+//
+
+import SwiftUI
+
+@main
+struct MultiplicationTablesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
